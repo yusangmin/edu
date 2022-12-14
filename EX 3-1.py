94d351954f6fa2f6b7a7302dbad324f69597b520
Yu Sang Min <smyu@nate.com>
2022-12-15 01:40:50 +0900

    wow -> hi

--- EX 3-1.py
+++ EX 3-1.py
@@ -98,3 +98,9 @@ velocity = float(input("물체의 속도를 입력하시오(미터/초): "))
  
 energy = 0.5 * mass * velocity **2
 print("물체는  " + str(energy) + " (줄)의 에너지를 가지고 있다.")
+# 3-8
+mass = float(input("물체의 무게를 입력하시오(킬로그램): "))
+velocity = float(input("물체의 속도를 입력하시오(미터/초): "))
+ 
+energy = 0.5 * mass * velocity **2
+print("물체는  " + str(energy) + " (줄)의 에너지를 가지고 있다.")
